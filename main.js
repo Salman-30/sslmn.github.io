f21dc7abfb473b87ea24b7753c11a854b929b77a
salman <136901367+Salman-30@users.noreply.github.com>
2024-11-20 11:10:49 +0700

    main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 alert('!_TIDAK BOLEH PROTES BILA WEBSITE JELEK KARENA DALAM TAHAP PENGEMBANGAN!')
  
+Print('hipotesis : semakin tinggi angka jomok pada lingkungan tersebut maka semakin rendah tinhkat zina pada linhkungan tersebut juga')
 
