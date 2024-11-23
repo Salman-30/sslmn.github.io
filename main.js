4090281ad39b2c9875bdf4d6ad8d60a54225ccd6
MrDrTomato <mrsirdrtomato@gmail.com>
2024-11-23 16:09:47 +0700

    Memperbaiki masalah

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 alert('!_TIDAK BOLEH PROTES BILA WEBSITE JELEK KARENA DALAM TAHAP PENGEMBANGAN!')
 let datebutton = document.getElementById("datebutton");
-datebutton.onclick=document.getElementById('demo').innerHTML = Date()
+function revealDate() {
+    document.getElementById("demo").innerHTML = Date()
+}
